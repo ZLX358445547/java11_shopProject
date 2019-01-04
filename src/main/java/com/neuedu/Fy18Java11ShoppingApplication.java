e00a64aa43a68e10547e8aa36641047212cbf31a
zhanglixing <358445547@qq.com>
2019-01-04 20:25:42 +0800

    first commit

---
+++ src/main/java/com/neuedu/Fy18Java11ShoppingApplication.java
@@ -0,0 +1,14 @@
+package com.neuedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fy18Java11ShoppingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Fy18Java11ShoppingApplication.class, args);
+    }
+
+}
+
